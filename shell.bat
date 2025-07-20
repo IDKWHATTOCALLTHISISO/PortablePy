e4a2497b1605d18bbc3b96bf40edfb0df66a3fa9
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-07-20 16:17:39 -0400

    Add files via upload

---
+++ shell.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "delims=" %%i in ('whoami') do set USER=%%i
+goto help-page
+
+:loop
+for /f "delims=" %%i in ('cd') do set WD=%%i
+set /p command="!USER!@!WD!: "
+if "!command!"=="" goto loop
+if "!command!"=="?" goto help-page
+if "!command!"=="exit" exit /b
+if "!command:~0,6!"=="python" goto disabled
+if "!command:~0,3!"=="pip" goto disabled
+!command!
+goto loop
+
+:help-page
+echo Use .\ if you want to run a command from the current directory
+echo Use the command exit to return to cmd
+echo run the command ? for help
+goto pyhelp
+
+:disabled
+echo Python and pip commands are disabled in this shell.
+goto pyhelp
+
+:pyhelp
+echo Pip is located at .\Scripts\pip and Python is at .\python
+goto loop
